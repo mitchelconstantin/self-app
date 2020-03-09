8c8e56762e56af3efff6bdc9a98f3d06138176b7
Mitchel Constantin <mitchconstantin@gmail.com>
2020-03-09 13:56:42 -0500

    implement validation and alert on submit

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Button, TextField, makeStyles } from '@material-ui/core';
 import whiteArrow from '../Assets/White_Arrow.svg';
 
@@ -32,33 +32,75 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const emptyState = {
+  firstName: '',
+  lastName: '',
+  address: '',
+  addressTwo: ''
+};
 export const Form = () => {
   const classes = useStyles();
+  const [formState, setFormState] = useState(emptyState);
+
+  const handleChange = (type: string, newValue: any) => {
+    setFormState(prev => ({ ...prev, [type]: newValue }));
+  };
+  const isValidForm = (): boolean =>
+    Boolean(formState.firstName && formState.lastName && formState.address);
+
+  const handleClick = () => {
+    const { firstName, lastName, address, addressTwo } = formState;
+    alert(
+      `
+      submitting this date:
+      first name : ${firstName}
+      last name : ${lastName}
+      address : ${address}
+      address two : ${addressTwo}
+      `
+    ); 
+    setFormState(emptyState);
+  };
   return (
     <Box className={classes.container}>
       <form className={classes.form} noValidate autoComplete="off">
         <TextField
           className={classes.textField}
+          required
+          value={formState.firstName}
+          onChange={e => handleChange('firstName', e.target.value)}
           label="FIRST NAME"
           variant="outlined"
         />
         <TextField
           className={classes.textField}
+          required
+          value={formState.lastName}
+          onChange={e => handleChange('lastName', e.target.value)}
           label="LAST NAME"
           variant="outlined"
         />
         <TextField
           className={classes.textField}
+          required
+          value={formState.address}
+          onChange={e => handleChange('address', e.target.value)}
           label="ADDRESS"
           variant="outlined"
         />
         <TextField
           className={classes.textField}
+          value={formState.addressTwo}
+          onChange={e => handleChange('addressTwo', e.target.value)}
           label="ADRESS 2 (OPTIONAL)"
           variant="outlined"
         />
       </form>
-      <Button className={classes.button}>
+      <Button
+        disabled={!isValidForm()}
+        onClick={handleClick}
+        className={classes.button}
+      >
         next <img src={whiteArrow} alt={'Logo'} />
       </Button>
     </Box>
