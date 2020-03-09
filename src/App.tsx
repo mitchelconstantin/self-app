21301a6ea68fc7ad0bab7c7f0c55a185b1496e28
Mitchel Constantin <mitchconstantin@gmail.com>
2020-03-09 12:20:42 -0500

    add typescript and material-ui to CRA, replace existing App.js with App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+export const App = () => {
+  return (
+    <>
+      <div>this is your form now</div>
+      <Button>click me</Button>
+    </>
+  );
+};
