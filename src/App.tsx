7eb923602717a4b676fcb5cbead09aeb7b129823
Mitchel Constantin <mitchconstantin@gmail.com>
2020-03-09 13:31:40 -0500

    layout header and form side by side, add basic styling to header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
+import { Box } from '@material-ui/core';
 import { Header } from './Components/Header';
 import { Form } from './Components/Form';
 
-
 export const App = () => {
   return (
-    <>
-    <Header />
-    <Form />
-    </>
+    <Box display='flex'>
+      <Header />
+      <Form />
+    </Box>
   );
 };
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
-import { Box, Button, TextField, makeStyles } from '@material-ui/core';
+import { Box, makeStyles, Typography } from '@material-ui/core';
 import giantRobot from '../Assets/GiantRobotLTD_Logo.svg';
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    backgroundColor: '#585858',
+    paddingTop: '32px',
+    paddingRight: '80px',
+    paddingLeft: '168px'
+    // height: '100%',
+  },
+  info: {
+    width: '535px'
+  },
+  logo: {
+    height: '26px',
+    width: '177px'
+  },
+  title: {
+    color: '#FFFFFF',
+    fontSize: '32px'
+  },
+  subTitle: {
+    color: '#EAEAEA ',
+    fontSize: '14px '
+  }
+}));
 
 export const Header = () => {
-  return <div>header</div>;
+  const classes = useStyles();
+  const title = 'Welcome';
+  const subTitle = 'Please tell us a bit about yourself to get started.';
+  return (
+    <Box className={classes.container}>
+      <Box className={classes.info}>
+        <img className={classes.logo} src={giantRobot} alt={'Logo'} />
+        <Typography className={classes.title}> {title}</Typography>
+        <Typography className={classes.subTitle}> {subTitle}</Typography>
+      </Box>
+    </Box>
+  );
 };
